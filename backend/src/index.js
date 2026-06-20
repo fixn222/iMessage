9356da8ed1e1d7301996e216479ab6dca27f1f56
fixn222 <176085692+fixn222@users.noreply.github.com>
2026-06-21 03:14:43 +0530

    Add Docker files , implement Express server with static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(clerkMiddleware());
 app.use(express.json());
@@ -17,6 +21,14 @@ app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+//if the public directory exsist , serve the static files
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
