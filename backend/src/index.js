e6269a421901906d17f5a7619d234c4a1df832e9
fixn222 <176085692+fixn222@users.noreply.github.com>
2026-06-20 11:58:30 +0530

    Add user model message model db conn intergrate clerk middlware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
 
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is up and running on http://localhost:${PORT}`);
 });
